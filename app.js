959547fafb4796b69c726b3f3748a53dd03aaa3e
nahsooyeon <nahsooyeon@gmail.com>
2021-04-06 18:22:15 +0900

    ⚒  수정 : app.js 스크립트 추가

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // require
 const express = require("express");
 const morgan = require("morgan");
+const summonerRouter = require("./routes/summoner");
 
 // 기본 설정
 const app = express();
@@ -8,9 +9,10 @@ app.use(morgan("dev"));
 
 // 라우트
 app.get("/", (req, res) => res.send("test"));
+app.use("/summoner", summonerRouter);
 
 // 서버 구동
 const PORT = 4000;
 app.listen(PORT, () => {
-	console.log(`Server is up on port ${PORT}`);
+  console.log(`Server is up on port ${PORT}`);
 });
