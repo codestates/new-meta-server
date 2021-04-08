9a881b434d5fdbb099f4a7fff2d9bfc61bf274d6
nahsooyeon <nahsooyeon@gmail.com>
2021-04-08 16:43:17 +0900

    ✨ 추가기능 : 소환사 최근 챔피언 사용 정보

--- controllers/summoner.js
+++ controllers/summoner.js
@@ -16,8 +16,12 @@ module.exports = {
         `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${API_KEY}`
       )
       .then((response) => {
-        console.log(response);
-        res.send(response.data);
+        res.status(200).json({
+          id: response.data.id,
+          accountId: response.data.accountId,
+          puuid: response.data.puuid,
+          name: response.data.name,
+        });
       })
       .catch((err) => res.status(404).send("Summoner Not Found"));
   },
@@ -40,21 +44,8 @@ module.exports = {
         }
       });
   },
-
-  summonerMatchList: (req, res) => {
+  summonerLaneInfo: (req, res) => {
     const encryptedAccountId = req.body.accountId;
-    axios
-      .get(
-        `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${encryptedAccountId}?queue=420&api_key=${API_KEY}`
-      )
-      .then((response) => {
-        res.json(response.data);
-      });
-  },
-
-  summonerLaneRatio: (req, res) => {
-    const encryptedAccountId = req.body.accountId;
-    console.log(encryptedAccountId);
 
     return axios
       .get(
@@ -62,7 +53,6 @@ module.exports = {
       )
       .then((response) => {
         const matchList = response.data.matches;
-        console.log(matchList.length);
         let laneCount = {
           TOP: 0,
           JUNGLE: 0,
@@ -78,12 +68,24 @@ module.exports = {
           } else if (matchList[i].lane === "MID") {
             laneCount.MID += 1;
           } else if (matchList[i].lane === "BOTTOM") {
-            laneCount.BOTTOM += 1;
+            laneCount.AD_CARRY += 1;
           } else if (matchList[i].lane === "NONE") {
             laneCount.SUPPORT += 1;
           }
         }
-        res.status(200).send(laneCount);
+        res.status(200).json(laneCount);
+      });
+  },
+
+  /* 최근 플레이한 챔피언의  */
+  summonerMatchList: (req, res) => {
+    const encryptedAccountId = req.body.accountId;
+    axios
+      .get(
+        `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${encryptedAccountId}?queue=420&api_key=${API_KEY}`
+      )
+      .then((response) => {
+        res.json(response.data);
       });
   },
 
@@ -92,17 +94,78 @@ module.exports = {
     try {
       const accountId = req.body.accountId;
       const summonerName = req.body.name;
-      const enCodeSummonerName = encodeURI(summonerName);
-      const getMatches = await axios
-        .get(
-          `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?endIndex=20&api_key=${API_KEY}&summonerName=${enCodeSummonerName}`
-        )
-        .then((response) => response.data);
-      const getMatchId = (matches) => {
-        matches.map((match) => {});
+      const enCodedSummonerName = encodeURI(summonerName);
+      const getChampionStats = async ({ gameId, champion }) => {
+        const summonerPlayInfo = await axios
+          .get(
+            `https://kr.api.riotgames.com/lol/match/v4/matches/${gameId}?api_key=${API_KEY}`
+          )
+          .then((response) => response.data)
+          .then((matchInfo) => matchInfo.participants)
+          .then((matchPlayerInfo) =>
+            matchPlayerInfo.filter((player) => {
+              return player.championId === champion;
+            })
+          );
+
+        let result = {
+          gameId: gameId,
+          champion: summonerPlayInfo[0].championId,
+          stats: {
+            participantId: summonerPlayInfo[0].stats.participantId,
+            win: summonerPlayInfo[0].stats.win,
+            kills: summonerPlayInfo[0].stats.kills,
+            deaths: summonerPlayInfo[0].stats.deaths,
+            assists: summonerPlayInfo[0].stats.assists,
+            lane: summonerPlayInfo[0].timeline.lane,
+          },
+        };
+
+        return result;
       };
+
+      const getMatchIDChampion = await axios
+        .get(
+          `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?endIndex=10&api_key=${API_KEY}&summonerName=${enCodedSummonerName}`
+        )
+        .then((response) => response.data.matches)
+        .then((matches) => {
+          let matchList = [];
+          matches.map((match) => {
+            matchList.push({ gameId: match.gameId, champion: match.champion });
+          });
+          return matchList;
+        })
+        .then(async (matchList) => {
+          // console.log("matchList", matchList);
+          let resultArray = [];
+
+          const callback = async () => {
+            for (let el of matchList) {
+              await getChampionStats(el).then((result) =>
+                resultArray.push(result)
+              );
+            }
+            return resultArray;
+          };
+          return callback();
+        });
+      res.status(200).json(getMatchIDChampion);
     } catch (err) {
       console.log(err);
     }
+  },
+
+  /* 15분까지의 타임라인 데이터  */
+
+  matchTimeline: (req, res) => {
+    const gameId = req.body.gameId;
+    axios
+      .get(
+        `https://kr.api.riotgames.com/lol/match/v4/timelines/by-match/${gameId}?api_key=${API_KEY}`
+      )
+      .then((response) => {
+        res.send(response.data.frames);
+      });
   },
 };
