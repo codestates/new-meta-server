4d041a0ce542d61fd1d035ed8a43b161efa8309f
Yang Haeseung <chachagogogo@gmail.com>
2021-04-09 16:03:59 +0900

    ⚒  수정 : user controller  추가

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { getRepository, getConnection } from "typeorm";
 import { User } from "../entity/User";
 import * as jwt from "jsonwebtoken";
+import * as bcrypt from "bcryptjs";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -9,62 +10,88 @@ class UserController {
 	static userCreate = async (req: Request, res: Response) => {
 		const { nickname, email, password } = req.body;
 
+		// 빈 요청값이 없는지 확인
+		if (!nickname || !email || !password) {
+			return res
+				.status(422)
+				.send({ message: "Nickname, email and password can't be blank" });
+		}
+
+		// 닉네임 중복검사
+		const checkNickname = await getRepository(User)
+			.createQueryBuilder()
+			.where({ nickname })
+			.getOne();
+		if (checkNickname) {
+			return res.status(409).send({ message: "Nickname already exists" });
+		}
+
+		// 이메일 중복검사
+		const checkEmail = await getRepository(User)
+			.createQueryBuilder()
+			.where({ email })
+			.getOne();
+		if (checkEmail) {
+			return res.status(409).send({ message: "Email already exists" });
+		}
+
+		// DB에 넣을 값
 		let user = new User();
 		user.nickname = nickname;
 		user.email = email;
 		user.password = password;
-
 		user.hashPassword();
 
-		const token = jwt.sign(
-			{ user: user.email.toString() },
-			process.env.TOKEN_SECRET
-		);
+		// INSERT 쿼리
+		const queryExecution = await getRepository(User)
+			.createQueryBuilder()
+			.insert()
+			.into(User)
+			.values(user)
+			.execute();
 
-		const userRepository = getRepository(User);
-		try {
-			const checkEmail = await userRepository.findOne({ email });
-			if (checkEmail) {
-				return res.status(409).send({ message: "email already exists" });
-			}
+		delete user.password;
 
-			const checkNickname = await userRepository.findOne({ nickname });
-			if (checkNickname) {
-				return res.status(409).send({ message: "nickname already exists" });
-			}
-
-			await userRepository.save(user);
-			delete user.password;
-		} catch (error) {
-			return res.status(500).send(error);
-		}
-		res.status(201).send({ user, token });
+		res.send({ user, message: "Account created successfully" });
 	};
 
 	static userLogin = async (req: Request, res: Response) => {
 		const { email, password } = req.body;
 
+		// 빈 요청값이 있는지 확인
 		if (!email || !password) {
-			res.status(400).send({ message: "email and password can't be blank" });
+			return res
+				.status(422)
+				.send({ message: "Email and password can't be blank" });
 		}
 
-		const userRepository = getRepository(User);
-		let user: User;
-		try {
-			user = await userRepository.findOneOrFail({ where: { email } });
-		} catch (error) {
-			res.status(401).send({ message: "unauthorized" });
+		// DB에서 해당 유저 정보 찾기
+		const user = await getRepository(User)
+			.createQueryBuilder()
+			.where({ email })
+			.getOne();
+
+		// 이메일에 해당하는 유저 있는지 확인
+		if (!user) {
+			return res.status(400).send({ message: "Check your email or password" });
 		}
 
+		// 비밀번호 맞는지 확인
 		if (!user.checkPassword(password)) {
-			res.status(401).send({ message: "unauthorized" });
-			return;
+			return res.status(400).send({ message: "Check your email or password" });
 		}
 
+		// 토큰 발급
 		const token = jwt.sign(
-			{ user: user.email.toString() },
+			{
+				id: user.id.toString(),
+				email: user.email.toString(),
+				nickname: user.nickname.toString(),
+			},
 			process.env.TOKEN_SECRET,
-			{ expiresIn: "1d" }
+			{
+				expiresIn: "15 days",
+			}
 		);
 
 		delete user.password;
@@ -72,17 +99,155 @@ class UserController {
 		res.send({ user, token });
 	};
 
-	// static userLogout = async (req: Request, res: Response) => {
-	// 	try {
-	// 		const userRepository = getRepository(User);
-	// 		let user: User;
-	// 		req.body.token = "";
-	// 		await userRepository.save(user);
-	// 		res.send();
-	// 	} catch (error) {
-	// 		res.status(500).send(error);
-	// 	}
-	// };
+	static userLogout = async (req: Request, res: Response) => {
+		const { email } = req.body;
+
+		// 토큰이 없는 경우
+		if (!req.headers.authorization) {
+			return res.status(401).send({ message: "Please login again" });
+		}
+
+		// 토큰 확인 된 경우
+		const token = req.headers.authorization.split(" ")[1];
+		try {
+			const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+			const user = await getRepository(User)
+				.createQueryBuilder()
+				.where({ email })
+				.getOne();
+
+			// 새 토큰 발급
+			const newToken = jwt.sign(
+				{ email: user.email.toString() },
+				process.env.TOKEN_SECRET,
+				{ expiresIn: "1s" }
+			);
+
+			delete user.password;
+
+			return res.send({ user, message: "Logged out successfully" });
+		} catch (error) {
+			// 토큰이 다른 경우
+			return res.status(400).send({ message: "Wrong access" });
+		}
+	};
+
+	static userReadProfile = async (req: Request, res: Response) => {
+		const { nickname, email, password } = req.body;
+
+		// 토큰이 없는 경우
+		if (!req.headers.authorization) {
+			return res.status(401).send({ message: "Please login again" });
+		}
+
+		// 토큰 확인 된 경우
+		const token = req.headers.authorization.split(" ")[1];
+		try {
+			const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+			const user = await getRepository(User)
+				.createQueryBuilder()
+				.where({ email })
+				.getOne();
+
+			delete user.password;
+
+			res.send({ user });
+		} catch (error) {
+			// 토큰이 다른 경우
+			return res.status(400).send({ message: "Wrong access" });
+		}
+	};
+
+	static userPasswordUpdate = async (req: Request, res: Response) => {
+		const { nickname, email, currentPassword, newPassword } = req.body;
+
+		// 공란 있는지 확인
+		if (!currentPassword || !newPassword) {
+			return res.status(400).send({
+				message: "Please enter both your current password and the new one",
+			});
+		}
+
+		// 현 비밀번호 제대로 입력했는지 확인
+		const passwordMatch = await getRepository(User)
+			.createQueryBuilder()
+			.where({ email })
+			.getOne();
+
+		console.log("user1", passwordMatch);
+
+		if (!passwordMatch.checkPassword(currentPassword)) {
+			return res
+				.status(400)
+				.send({ message: "Please check your current password" });
+		}
+
+		// 토큰이 없는 경우
+		if (!req.headers.authorization) {
+			return res.status(401).send({ message: "Please login again" });
+		}
+
+		// 토큰 확인
+		const token = req.headers.authorization.split(" ")[1];
+		let decoded: any;
+		try {
+			decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+		} catch (error) {
+			// 토큰이 다른 경우
+			return res.status(400).send({ message: "Wrong access" });
+		}
+
+		const hashedNewPassword = bcrypt.hashSync(newPassword, 8);
+		const user = await getRepository(User)
+			.createQueryBuilder()
+			.update(User)
+			.set({ password: hashedNewPassword })
+			.where({ email })
+			.execute();
+
+		console.log("user2", user);
+		res.send({ user });
+	};
+
+	static userDelete = async (req: Request, res: Response) => {
+		const { email, password } = req.body;
+
+		// 토큰이 없는 경우
+		if (!req.headers.authorization) {
+			res.status(401).send({ message: "Please login again" });
+		}
+
+		// 현 비밀번호 제대로 입력했는지 확인
+		const user = await getRepository(User)
+			.createQueryBuilder()
+			.where({ email })
+			.getOne();
+
+		if (!user.checkPassword(password)) {
+			return res
+				.status(400)
+				.send({ message: "Please check your current password" });
+		}
+
+		// 토큰 확인
+		const token = req.headers.authorization.split(" ")[1];
+		try {
+			const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+			const user = await getRepository(User)
+				.createQueryBuilder()
+				.delete()
+				.where({ email })
+				.execute();
+
+			res.send({ message: "Account deleted successfully" });
+		} catch (error) {
+			// 토큰이 다른 경우
+			return res.status(400).send({ message: "Wrong access" });
+		}
+	};
 }
 
 export default UserController;
