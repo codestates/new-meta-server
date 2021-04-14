239f6a676a2fcd4fd751ee78e0f23737ea8c8d7c
osunguk(mac) <osunguk@gmail.com>
2021-04-14 23:55:54 +0900

    🔨 수정 : server 개발환경 세팅

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,11 +1,11 @@
 import { Field, ObjectType } from "type-graphql";
 import {
-	BaseEntity,
-	Column,
-	Entity,
-	OneToMany,
-	OneToOne,
-	PrimaryGeneratedColumn,
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
 } from "typeorm";
 
 import { OpenAuth } from "./OpenAuth";
@@ -16,37 +16,37 @@ import { Like } from "./Like";
 @ObjectType()
 @Entity({ name: "users" })
 export class User extends BaseEntity {
-	@Field()
-	@PrimaryGeneratedColumn("uuid")
-	id: string;
+  @Field()
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-	@Field()
-	@Column()
-	nickname!: string;
+  @Field()
+  @Column()
+  nickname!: string;
 
-	@Field()
-	@Column()
-	email!: string;
+  @Field()
+  @Column()
+  email!: string;
 
-	@Column()
-	password!: string;
+  @Column()
+  password!: string;
 
-	@OneToOne(() => OpenAuth, (openAuth) => openAuth.user)
-	openAuth?: OpenAuth;
+  @OneToOne(() => OpenAuth, (openAuth) => openAuth.user)
+  openAuth?: OpenAuth;
 
-	@Field(() => [Post], { nullable: true })
-	@OneToMany(() => Post, (post) => post.user, { nullable: true })
-	posts: Post[] | null;
+  @Field(() => [Post], { nullable: true })
+  @OneToMany(() => Post, (post) => post.user, { nullable: true })
+  posts: Post[] | null;
 
-	@Field(() => [Bookmark], { nullable: true })
-	@OneToMany(() => Bookmark, (bookmark) => bookmark.follower)
-	followerIds: Bookmark[];
+  @Field(() => [Bookmark], { nullable: true })
+  @OneToMany(() => Bookmark, (bookmark) => bookmark.follower)
+  followerIds: Bookmark[];
 
-	@Field(() => [Bookmark], { nullable: true })
-	@OneToMany(() => Bookmark, (bookmark) => bookmark.followee)
-	followeeIds: Bookmark[];
+  @Field(() => [Bookmark], { nullable: true })
+  @OneToMany(() => Bookmark, (bookmark) => bookmark.followee)
+  followeeIds: Bookmark[];
 
-	@Field(() => [Like], { nullable: true })
-	@OneToMany(() => Like, (like) => like.user)
-	likes: Like[];
+  @Field(() => [Like], { nullable: true })
+  @OneToMany(() => Like, (like) => like.user)
+  likes: Like[];
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import user from "./user";
 import summoner from "./summoner";
 import staticData from "./static";
 
 const routes = Router();
 
-routes.use("/users", user);
 routes.use("/summoners", summoner);
 routes.use("/statics", staticData);
 
