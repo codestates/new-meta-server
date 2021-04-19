7877c62ef2d3af3fc32f16bb90f0a31453efd0ed
Yang Haeseung <chachagogogo@gmail.com>
2021-04-19 17:12:37 +0900

    🔨 수정 : postResolver 추가 및 post 관련 타입 정리

--- src/resolvers/postResolvers.ts
+++ src/resolvers/postResolvers.ts
@@ -7,6 +7,7 @@ import {
 	UseMiddleware,
 } from "type-graphql";
 
+import { User } from "../entities/User";
 import { Post } from "../entities/Post";
 import { isAuth } from "./middleware/isAuth";
 
@@ -15,6 +16,10 @@ import {
 	CreatePostInputType,
 	CreatePostResponseType,
 } from "./types/postTypes/CreatePostType";
+import {
+	UpdatePostInputType,
+	UpdatePostResponseType,
+} from "./types/postTypes/UpdatePostType";
 
 @Resolver()
 export class CreatePostResolver {
@@ -49,5 +54,72 @@ export class CreatePostResolver {
 		if (!posts) throw new Error("Item not found");
 
 		return posts;
+	}
+
+	@Query(() => [Post], { nullable: true })
+	async readOthersPosts(@Arg("email") email: string) {
+		const arrayedUserInfo = await User.find({
+			where: { email },
+		});
+
+		const [userInfo] = arrayedUserInfo;
+
+		const posts = await Post.find({
+			where: {
+				user: userInfo.id,
+			},
+		});
+
+		return posts;
+	}
+
+	@Mutation(() => UpdatePostResponseType)
+	@UseMiddleware(isAuth)
+	async updatePost(
+		@Arg("data") data: UpdatePostInputType,
+		@Ctx() { payload }: MyContext
+	) {
+		const arrayedPost = await Post.find({
+			where: {
+				user: payload?.userId,
+				id: data.id,
+			},
+		});
+		if (arrayedPost.length === 0) throw new Error("Post not found");
+
+		const [post] = arrayedPost;
+
+		const newPost = {
+			...data,
+			user: {
+				id: payload!.userId,
+				email: payload?.userEmail,
+				nickname: payload?.userNickname,
+			},
+		};
+
+		await Object.assign(post, newPost).save();
+
+		return { ...newPost };
+	}
+
+	@Mutation(() => Boolean, { nullable: true })
+	@UseMiddleware(isAuth)
+	async deletePost(
+		@Arg("postId") postId: string,
+		@Ctx() { payload }: MyContext
+	) {
+		const arrayedPost = await Post.find({
+			where: {
+				id: postId,
+				user: payload?.userId,
+			},
+		});
+		if (arrayedPost.length === 0) throw new Error("Something went wrong.");
+
+		const [post] = arrayedPost;
+
+		await post.remove();
+		return true;
 	}
 }
