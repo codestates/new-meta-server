592de80b1d7d8901a4db8e13b15fd236bd0d378b
Yang Haeseung <chachagogogo@gmail.com>
2021-04-22 17:06:11 +0900

    🔨 수정 : 환경변수 파일 미사용 코드 삭제, index.ts 불필요한 주석 삭제 등

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,96 @@
+import https from "https";
+import fs from "fs";
+
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 import cors from "cors";
-import dotenv from "dotenv";
-import routes from "./routes";
-dotenv.config();
-import https from "https";
-import fs from "fs";
+import "dotenv/config";
 
-// import { send } from "process";
+import routes from "./routes";
 
 const main = async () => {
-  const app = express();
+	const app = express();
 
-  if (process.env.NODE_ENV === "development") {
-    // config.development;
-    await createConnection({
-      type: "mysql",
-      host: "127.0.0.1",
-      // port: process.env.DB_PORT,
-      username: process.env.LOCAL_DATABASE_USERNAME,
-      password: process.env.LOCAL_DATABASE_PASSWORD,
-      database: process.env.LOCAL_DATABASE_NAME,
-      synchronize: true,
-      logging: false,
-      entities: ["src/entities/*.*"],
-    });
-  } else if (process.env.NODE_ENV === "deploy") {
-    // const temp = config.deploy;
-    await createConnection({
-      type: "mysql",
-      host: process.env.DB_HOST,
-      // port: process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DBNAME,
-      synchronize: true,
-      logging: true,
-      entities: ["src/entities/*.*"],
-    });
-  }
+	if (process.env.NODE_ENV === "development") {
+		await createConnection({
+			type: "mysql",
+			host: "127.0.0.1",
+			// port: process.env.DB_PORT,
+			username: process.env.LOCAL_DATABASE_USERNAME,
+			password: process.env.LOCAL_DATABASE_PASSWORD,
+			database: process.env.LOCAL_DATABASE_NAME,
+			synchronize: true,
+			logging: false,
+			entities: ["src/entities/*.*"],
+		});
+	} else if (process.env.NODE_ENV === "production") {
+		await createConnection({
+			type: "mysql",
+			host: process.env.DB_HOST,
+			// port: process.env.DB_PORT,
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DBNAME,
+			synchronize: true,
+			logging: true,
+			entities: ["src/entities/*.*"],
+		});
+	}
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
 
-  const schema = await buildSchema({
-    resolvers: [__dirname + "/resolvers/*.ts"],
-  });
+	const schema = await buildSchema({
+		resolvers: [__dirname + "/resolvers/*.ts"],
+	});
 
-  const apolloServer = new ApolloServer({
-    schema,
-    context: ({ req, res }: any) => ({ req, res }),
-  });
+	const apolloServer = new ApolloServer({
+		schema,
+		context: ({ req, res }: any) => ({ req, res }),
+	});
 
-  app.use(
-    cors({
-      credentials: true,
-      origin: true,
-      methods: ["GET", "POST", "OPTIONS"],
-    })
-  );
+	app.use(
+		cors({
+			credentials: true,
+			origin: true,
+			methods: ["GET", "POST", "OPTIONS"],
+		})
+	);
 
-  app.use("/", routes);
+	app.use("/", routes);
 
-  // apolloServer.start();
-  apolloServer.applyMiddleware({ app });
+	apolloServer.applyMiddleware({ app });
 
-  const PORT = 4000;
-  let server;
-  if (
-    fs.existsSync("/etc/letsencrypt/live/new-meta.ga/") &&
-    fs.existsSync("/etc/letsencrypt/live/new-meta.ga/")
-  ) {
-    const privateKey = fs.readFileSync(
-      "/etc/letsencrypt/live/new-meta.ga/privkey.pem",
-      "utf8"
-    );
-    const certificate = fs.readFileSync(
-      "/etc/letsencrypt/live/new-meta.ga/cert.pem",
-      "utf8"
-    );
+	// apolloServer.start();
+	const PORT = 4000;
+	let server;
+	if (
+		fs.existsSync("/etc/letsencrypt/live/new-meta.ga/") &&
+		fs.existsSync("/etc/letsencrypt/live/new-meta.ga/")
+	) {
+		const privateKey = fs.readFileSync(
+			"/etc/letsencrypt/live/new-meta.ga/privkey.pem",
+			"utf8"
+		);
+		const certificate = fs.readFileSync(
+			"/etc/letsencrypt/live/new-meta.ga/cert.pem",
+			"utf8"
+		);
 
-    const credentials = { key: privateKey, cert: certificate };
-    server = https.createServer(credentials, app);
-    server.listen(PORT, () =>
-      console.log(`🚀 HTTPS Server is starting on ${PORT}/graphql`)
-    );
-  } else {
-    server = app.listen(PORT);
-    console.log(`🚀 HTTP Server is starting on ${PORT}/graphql`);
-  }
+		const credentials = { key: privateKey, cert: certificate };
+		server = https.createServer(credentials, app);
+		server.listen(PORT, () =>
+			console.log(`🚀 HTTPS Server is starting on ${PORT}/graphql`)
+		);
+	} else {
+		server = app.listen(PORT);
+		console.log(`🚀 HTTP Server is starting on ${PORT}/graphql`);
+	}
 
-  // app.listen(PORT, () => console.log(`Server is up on port ${PORT}/graphql`));
+	// app.listen(PORT, () => console.log(`Server is up on port ${PORT}/graphql`));
 };
 
 main();
