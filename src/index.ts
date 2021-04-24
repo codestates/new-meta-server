10f260323557bac7c620a0ba4265ec4078543f59
Yang Haeseung <chachagogogo@gmail.com>
2021-04-24 15:30:44 +0900

    ✨ 추가기능 : 패스포트에 깃헙 등 추가

--- src/index.ts
+++ src/index.ts
@@ -77,10 +77,32 @@ const main = async () => {
 		"/auth/google",
 		passport.authenticate("google", { scope: ["profile"] })
 	);
-
 	app.get(
 		"/auth/google/callback",
 		passport.authenticate("google", { failureRedirect: "/login" }),
+		function (req, res) {
+			// Successful authentication, redirect home.
+			res.redirect("/");
+		}
+	);
+
+	app.get("/auth/facebook", passport.authenticate("facebook"));
+	app.get(
+		"/auth/facebook/callback",
+		passport.authenticate("facebook", { failureRedirect: "/login" }),
+		function (req, res) {
+			// Successful authentication, redirect home.
+			res.redirect("/");
+		}
+	);
+
+	app.get(
+		"/auth/github",
+		passport.authenticate("github", { scope: ["user:email"] })
+	);
+	app.get(
+		"/auth/github/callback",
+		passport.authenticate("github", { failureRedirect: "/login" }),
 		function (req, res) {
 			// Successful authentication, redirect home.
 			res.redirect("/");
