826e028908997cc0bf5d84d97f7c06243b7d77e4
Yeonju Lee <jessy2yon@gmail.com>
2021-05-02 02:27:56 +0900

    ⚒  수정 : 도메인 변경

--- src/index.ts
+++ src/index.ts
@@ -16,97 +16,97 @@ import passport from "./lib/passport";
 import routes from "./routes";
 
 const main = async () => {
-	const app = express();
+  const app = express();
 
-	// Database와 연결
-	if (process.env.NODE_ENV === "development") {
-		await createConnection({
-			type: "mysql",
-			host: "127.0.0.1",
-			// port: process.env.DB_PORT,
-			username: process.env.LOCAL_DATABASE_USERNAME,
-			password: process.env.LOCAL_DATABASE_PASSWORD,
-			database: process.env.LOCAL_DATABASE_NAME,
-			synchronize: true,
-			logging: false,
-			entities: ["src/entities/*.*"],
-		});
-	} else if (process.env.NODE_ENV === "production") {
-		await createConnection({
-			type: "mysql",
-			host: process.env.DB_HOST,
-			// port: process.env.DB_PORT,
-			username: process.env.DB_USERNAME,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_DBNAME,
-			synchronize: true,
-			logging: true,
-			entities: ["src/entities/*.*"],
-		});
-	}
+  // Database와 연결
+  if (process.env.NODE_ENV === "development") {
+    await createConnection({
+      type: "mysql",
+      host: "127.0.0.1",
+      // port: process.env.DB_PORT,
+      username: process.env.LOCAL_DATABASE_USERNAME,
+      password: process.env.LOCAL_DATABASE_PASSWORD,
+      database: process.env.LOCAL_DATABASE_NAME,
+      synchronize: true,
+      logging: false,
+      entities: ["src/entities/*.*"],
+    });
+  } else if (process.env.NODE_ENV === "production") {
+    await createConnection({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      // port: process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DBNAME,
+      synchronize: true,
+      logging: true,
+      entities: ["src/entities/*.*"],
+    });
+  }
 
-	// 각종 미들웨어
-	morgan.token("graphql-query", (req: any) => {
-		const { query, variables, operationName } = req.body;
-		return `GRAPHQL: \nOperation Name: ${operationName} \nQuery: ${query} \nVariables: ${JSON.stringify(
-			variables
-		)}`;
-	});
-	app.use(morgan(":graphql-query")); // GraphQL용 로거
-	app.use(morgan("common")); // RESTful API용 로거
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: false }));
-	app.use(passport.initialize());
-	app.use(
-		cors({
-			credentials: true,
-			origin: true,
-			methods: ["GET", "POST", "OPTIONS"],
-		})
-	);
+  // 각종 미들웨어
+  morgan.token("graphql-query", (req: any) => {
+    const { query, variables, operationName } = req.body;
+    return `GRAPHQL: \nOperation Name: ${operationName} \nQuery: ${query} \nVariables: ${JSON.stringify(
+      variables
+    )}`;
+  });
+  app.use(morgan(":graphql-query")); // GraphQL용 로거
+  app.use(morgan("common")); // RESTful API용 로거
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(passport.initialize());
+  app.use(
+    cors({
+      credentials: true,
+      origin: true,
+      methods: ["GET", "POST", "OPTIONS"],
+    })
+  );
 
-	// 스키마 설정
-	const schema = await buildSchema({
-		resolvers: [__dirname + "/resolvers/*.ts"],
-	});
+  // 스키마 설정
+  const schema = await buildSchema({
+    resolvers: [__dirname + "/resolvers/*.ts"],
+  });
 
-	// 아폴로 서버 구동
-	const apolloServer = new ApolloServer({
-		schema,
-		context: ({ req, res }: any) => ({ req, res }),
-	});
+  // 아폴로 서버 구동
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req, res }: any) => ({ req, res }),
+  });
 
-	// 라우트
-	app.use("/", routes);
+  // 라우트
+  app.use("/", routes);
 
-	// 아폴로서버 미들웨어
-	apolloServer.applyMiddleware({ app });
+  // 아폴로서버 미들웨어
+  apolloServer.applyMiddleware({ app });
 
-	// 서버 구동
-	const PORT = 4000;
-	let server;
-	if (
-		fs.existsSync("/etc/letsencrypt/live/new-meta.ga/") &&
-		fs.existsSync("/etc/letsencrypt/live/new-meta.ga/")
-	) {
-		const privateKey = fs.readFileSync(
-			"/etc/letsencrypt/live/new-meta.ga/privkey.pem",
-			"utf8"
-		);
-		const certificate = fs.readFileSync(
-			"/etc/letsencrypt/live/new-meta.ga/cert.pem",
-			"utf8"
-		);
+  // 서버 구동
+  const PORT = 4000;
+  let server;
+  if (
+    fs.existsSync("/etc/letsencrypt/live/new-meta.club/") &&
+    fs.existsSync("/etc/letsencrypt/live/new-meta.club/")
+  ) {
+    const privateKey = fs.readFileSync(
+      "/etc/letsencrypt/live/new-meta.club/privkey.pem",
+      "utf8"
+    );
+    const certificate = fs.readFileSync(
+      "/etc/letsencrypt/live/new-meta.club/cert.pem",
+      "utf8"
+    );
 
-		const credentials = { key: privateKey, cert: certificate };
-		server = https.createServer(credentials, app);
-		server.listen(PORT, () =>
-			logger.info(`🚀 HTTPS Server is starting on ${PORT}`)
-		);
-	} else {
-		server = app.listen(PORT);
-		console.log(`🚀 HTTP Server is starting on ${PORT}`);
-	}
+    const credentials = { key: privateKey, cert: certificate };
+    server = https.createServer(credentials, app);
+    server.listen(PORT, () =>
+      logger.info(`🚀 HTTPS Server is starting on ${PORT}`)
+    );
+  } else {
+    server = app.listen(PORT);
+    console.log(`🚀 HTTP Server is starting on ${PORT}`);
+  }
 };
 
 main();
